89daa1cb88bda9f7958da5e0d280089b7151eaa6
Максим <79196187+MaxKillMax@users.noreply.github.com>
2021-12-15 01:23:26 +0500

    fix

--- Scripts/Camera/CameraController.cs
+++ Scripts/Camera/CameraController.cs
@@ -2,16 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private CameraMovement _cameraMovement;
+    
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 direction = new Vector3(horizontal, scroll*-300, vertical);
-        _cameraMovement.Move(direction);
-        _cameraMovement.CheckHeight();
+        
+        if (horizontal != 0 || vertical != 0)
+        {
+            Move(horizontal, vertical, scroll);
+        }
+        
+        if (scroll != 0)
+        {
+            SetHeight(scroll);
+        }
+    }
+    
+    private void Move(float horizontal, float vertical, float scroll)
+    {
+        float scrollMultiply = -300;
+        float speed = 10;
+    
+        Vector3 direction = new Vector3(horizontal, scroll * scrollMultiply, vertical);
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    private void SetHeight(float scrollValue)
+    {
+        float scrollMultiply = 4;
+        float cameraMinRotateY = 15
+    
+        float cameraMaxY = 22;
+        float cameraMinY = 2;
+    
+        if (transform.position.y > minRotateValueY)
+        {
+            transform.rotation = Quaternion.Euler(transform.position.y * scrollValue * scrollMultiply, 0, 0);
+            
+            if (transform.position.y >= cameraMaxY)
+            {
+                transform.position = new Vector3(transform.position.x, cameraMaxY, transform.position.z);
+                transform.rotation = Quaternion.Euler(90, 0, 0);
+            }
+        }
+        else
+        {
+            if (transform.position.y <= cameraMinY)
+            {
+                transform.position = new Vector3(transform.position.x, cameraMinY, transform.position.z);
+            }
+        }
+        
     }
 }
